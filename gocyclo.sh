f64ba276663e520f2f3c31fddf7d09bbb7f31d3a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-05-18 14:21:46 +0200

    Print the path to gocyclo binary

--- gocyclo.sh
+++ gocyclo.sh
@@ -18,6 +18,7 @@ if ! [ -x "$(command -v gocyclo)" ]
 then
     echo -e "${BLUE}Installing gocyclo${NC}"
     GO111MODULE=off go get github.com/fzipp/gocyclo/cmd/gocyclo
+    which gocyclo
 fi
 
 gocyclo -over 9 -avg .
